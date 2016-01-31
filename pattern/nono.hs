a0214872509c791b792f4fd901fef38d324852e4
Mario Wenzel <maweki@gmail.com>
2016-01-31 13:59:51 +0100

    Added Problem type and extracted code

--- pattern/nono.hs
+++ pattern/nono.hs
@@ -7,6 +7,14 @@ import Control.Monad (replicateM, forM_)
 
 import Ersatz
 
+type PSize = (Int, Int)
+type Run = [[Int]]
+type Runs = (Run, Run)
+data Problem = Problem PSize Runs deriving Show
+
+problem_size :: Problem -> PSize
+problem_size (Problem size _) = size
+
 build_runs :: [Int] -> [Bit] -> Bit
 build_runs nums vars = case nums of
   [] -> all not vars
@@ -19,30 +27,31 @@ build_runs nums vars = case nums of
               && (all id $ zipWith id run $ take (x + 2) $ drop d v)
               && (build_runs xs $ drop (x + d + 1) vars)
 
+
 main = do
   input <- getContents
   let (xin, yin) = read input :: ([[Int]],[[Int]])
-      sizex = length xin
-      sizey = length yin
-      indices = [(x, y) | x <- [1..sizex], y <- [1..sizey] ]
+      problem = Problem (length xin, length yin) (xin, yin)
 
-      nono :: (MonadState s m, HasSAT s) =>  m (Map (Int,Int) Bit)
-      nono = do
-        vars' <- replicateM (length indices) exists
-        vars <- return $ Map.fromList $ zip indices vars'
-        let getx x = for [1..sizey] $ \y -> (vars ! (x,y))
-            gety y = for [1..sizex] $ \x -> (vars ! (x,y))
-            xs = zipWith build_runs xin (map getx [1..])
-            ys = zipWith build_runs yin (map gety [1..])
-        assert (all id xs && all id ys)
-        return vars
+  putStrLn $ "Size: " ++ (show $ problem_size problem)
 
-  putStrLn $ "Size: " ++ show (sizex, sizey)
-
-  (Satisfied, Just solution) <- solveWith minisat nono
-  let lines = for [1..sizey] $ \i -> filter (\ ((x, y), b) -> y == i) $ toList solution
+  (Satisfied, Just solution) <- solveWith minisat (nono problem)
+  let sizey = snd $ problem_size problem
+      lines = for [1..sizey] $ \i -> filter (\ ((x, y), b) -> y == i) $ toList solution
       conv  = map $ \((x,y), b)-> if b then 'X' else ' '
       join = foldr (:) ""
   forM_ (map (join . conv) lines) print
+
+nono :: (MonadState s m, HasSAT s) => Problem -> m (Map (Int,Int) Bit)
+nono (Problem (sizex, sizey) (xin, yin)) = do
+  let indices = [(x, y) | x <- [1..sizex], y <- [1..sizey] ]
+  vars' <- replicateM (length indices) exists
+  vars <- return $ Map.fromList $ zip indices vars'
+  let getx x = for [1..sizey] $ \y -> (vars ! (x,y))
+      gety y = for [1..sizex] $ \x -> (vars ! (x,y))
+      xs = zipWith build_runs xin (map getx [1..])
+      ys = zipWith build_runs yin (map gety [1..])
+  assert (all id xs && all id ys)
+  return vars
 
 for = flip map
